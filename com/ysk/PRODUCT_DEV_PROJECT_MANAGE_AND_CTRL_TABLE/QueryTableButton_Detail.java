4b976dc2c33398145754343e32de0f710f1d84f8
B0050 <B0050@yungshingroup.com>
2015-11-30 16:31:50 +0800

    增加詳細資料顯示按鈕功能。

--- com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/QueryTableButton_Detail.java
+++ com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/QueryTableButton_Detail.java
@@ -2,7 +2,9 @@ package com.ysk.PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE;
 
 //com/ysk/LAB_RECBOOK_USING_APPLY/QueryTableButton_Detail;
 import SomeUtils._bproc;
+import SomeUtils.Bean.ProductDevProjectTableBean;
 import SomeUtils.Bean.UserInfoViewBean;
+import SomeUtils.DAO.ProductDevProjectTableDAO;
 
 /**
  * 用來在表格中做為按鈕,顯示詳細資料.<br>
@@ -16,21 +18,35 @@ public class QueryTableButton_Detail extends _bproc {
 	public String getDefaultValue(String arg0) throws Throwable {
 		// 0:id 1:name
 		String[] empIdName = getValue("QUERY_LIST.REQ_EMPID").split("-");
+		ProductDevProjectTableDAO pTableDAO = new ProductDevProjectTableDAO(getTalk());
+		ProductDevProjectTableBean pbean = pTableDAO.getProductDevProjectTableBean(getValue("QUERY_LIST.PNO").trim());
 		// TODO Auto-generated method stub
-		UserInfoViewBean user = getUserInfo(empIdName[0].trim());
 		//message(user.getEmpid());
-		setValue("PNO", getValue("QUERY_LIST.PNO"));
-		setValue("REQ_EMPID", user.getEmpid());
-		setValue("REC_START_DATE", getValue("QUERY_LIST.REC_START_DATE"));
-		setValue("REC_END_DATE", getValue("QUERY_LIST.REC_END_DATE"));
-		setValue("RECBOOK_NAME", getValue("QUERY_LIST.RECBOOK_NAME"));
-		setValue("RECBOOK_NO", getValue("QUERY_LIST.RECBOOK_NO"));
-
-		setValue("REQ_EMPID_NAME", empIdName[1]);
-		setValue("REQ_DEPT_NAME", user.getDepName());
-		setVisible("DoAdd", false);
+		setValue("PNO", getValue("QUERY_LIST.PNO").trim());
+		setValue("DATE", pbean.getDATE().trim());
+		setValue("P_NAME", pbean.getP_NAME().trim());
+		setValue("DESCRIPTION", pbean.getDESCRIPTION().trim());
+		setValue("EXP_DATE", pbean.getEXP_DATE().trim());
+		setValue("GENERIC_CNAME", pbean.getGENERIC_CNAME().trim());
+		setValue("GENERIC_ENAME", pbean.getGENERIC_ENAME().trim());
+		setValue("DOSAGE_FORM", pbean.getDOSAGE_FORM().trim());
+		setValue("DOSE", pbean.getDOSE().trim());
+		setValue("PACKING", pbean.getPACKING().trim());
+		setValue("DEV_TYPE", pbean.getDEV_TYPE().trim());
+		setValue("MEDICINE_TYPE",pbean.getMEDICINE_TYPE().trim());
+		setValue("SALES_ATTACHED", pbean.getSALES_ATTACHED().trim());
+		setValue("LAW_ATTACHED", pbean.getLAW_ATTACHED().trim());
+		setValue("PURCH_ATTACHED", pbean.getPURCH_ATTACHED().trim());
+		setValue("PROCESS_ATTACHED", pbean.getPROCESS_ATTACHED().trim());
+		setValue("RD_ATTACHED", pbean.getRD_ATTACHED().trim());
+		setValue("IS_APPROVE", pbean.getIS_APPROVE().trim());
+		setValue("PROJECT_NO", pbean.getPROJECT_NO().trim());
+		setValue("REQ_EMPID", pbean.getREQ_EMPID().trim());
+		UserInfoViewBean user = getUserInfo(pbean.getREQ_EMPID().trim());
+		setValue("REQ_EMPID_NAME",user.getHecname().trim());
+		setValue("REQ_DEPT_NAME",user.getDepName().trim());
 		// QUERY SEND COMPUTE
-
+		setVisible("DoAdd",false);
 		return null;
 	}
 
