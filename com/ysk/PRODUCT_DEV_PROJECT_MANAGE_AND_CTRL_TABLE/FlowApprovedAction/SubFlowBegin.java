e6d98e1d2a80cd96d27cd2b8d2bd641c9c16a744
B0050 <B0050@yungshingroup.com>
2015-12-14 15:19:28 +0800

    當主流程到達相關單位簽核時,將自動於子流程起五張單 同時跑五條獨立流程,當中資料讀寫都是針對主單.

--- com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/FlowApprovedAction/SubFlowBegin.java
+++ com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/FlowApprovedAction/SubFlowBegin.java
@@ -1,13 +1,17 @@
 package com.ysk.PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE.FlowApprovedAction;
 
 //com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/FlowApprovedAction/SubFlowBegin
+import java.util.ArrayList;
+
+import jcx.db.talk;
 import jcx.jform.bProcFlow;
-import SomeUtils.Bean.ProductDevProjectTableBean;
-import SomeUtils.DAO.ProductDevProjectTableDAO;
+import SomeUtils.Bean.SubProductDevProjectTableBean;
+import SomeUtils.DAO.SubProductDevProjectTableDAO;
+
 /**
- * 程式起單.
- * 新增5筆資料 in SUB_PRODUCT_DEV_PROJECT_TABLE.
- * 並設定簽核狀態  in SUB_PRODUCT_DEV_PROJECT_TABLE_FLOWC.
+ * 程式起單. 新增5筆資料 in SUB_PRODUCT_DEV_PROJECT_TABLE. 並設定簽核狀態 in
+ * SUB_PRODUCT_DEV_PROJECT_TABLE_FLOWC.
+ * 
  * @author YangTing
  *
  */
@@ -18,21 +22,20 @@ public class SubFlowBegin extends bProcFlow {
 	@Override
 	public boolean action(String paramString) throws Throwable {
 		// TODO Auto-generated method stub
+		// 取得主單單號
+		talk t = getTalk();
+		String MSATER_PNO = getValue("PNO").trim();
+		SubProductDevProjectTableDAO sDao = new SubProductDevProjectTableDAO(t);
+		ArrayList<SubProductDevProjectTableBean> blist = new ArrayList<SubProductDevProjectTableBean>();
 
-		String pno = getValue("PNO");
-		ProductDevProjectTableDAO dao = new ProductDevProjectTableDAO(getTalk());
+		// 起5張子單,分別跑營業 質量 採購 作業 研發等部門 .
+		for (int i = 1; i <= 5; i++) {
+			blist.add(new SubProductDevProjectTableBean(Integer.toString(i), MSATER_PNO));
+			t.execFromPool("");
+		}	
 
-		ProductDevProjectTableBean bean = dao
-				.getProductDevProjectTableBean(pno);
-
-		if (getValue("PROJECT_NO").trim().length() != 0) {
-
-			bean.setPROJECT_NO(getValue("PROJECT_NO").trim());
-			dao.update(bean);
-			dao = null;
-			return true;
-		}
-		message("專案編號欄位必須輸入!");
-		return false;
+		sDao.add(blist);
+		return true;
 	}
+	
 }
