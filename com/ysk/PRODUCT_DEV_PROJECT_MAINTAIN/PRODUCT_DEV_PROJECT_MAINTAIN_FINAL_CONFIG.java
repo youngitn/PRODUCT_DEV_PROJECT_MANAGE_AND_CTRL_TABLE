a97be22d11a4378e55f9eddbe5c9655b95a49059
B0050 <B0050@yungshingroup.com>
2015-12-24 16:32:01 +0800

    靜態常數用

---
+++ com/ysk/PRODUCT_DEV_PROJECT_MAINTAIN/PRODUCT_DEV_PROJECT_MAINTAIN_FINAL_CONFIG.java
@@ -0,0 +1,9 @@
+package com.ysk.PRODUCT_DEV_PROJECT_MAINTAIN;
+/**
+ * 用於取得簽核人員的規則表,當中資料成員涉為int,方便使用switch.
+ */
+public interface PRODUCT_DEV_PROJECT_MAINTAIN_FINAL_CONFIG {
+	
+	//當MAINTAIN_TAYPE = 1 且簽核狀態 = 歸檔 且不再簽核流程中
+	final static String MAINTAIN_OK_STAT = "歸檔";
+}
