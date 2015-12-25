a951464f4ae3d7f5f91eb4ce52b17230a00a21fc
B0050 <B0050@yungshingroup.com>
2015-12-25 16:15:35 +0800

    增加 POSITION == 5 條件判斷,避免未知 nullpointer expection

--- com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/Init.java
+++ com/ysk/PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE/Init.java
@@ -4,8 +4,8 @@ package com.ysk.PRODUCT_DEV_PROJECT_MANAGE_AND_CTRL_TABLE;
 import SomeUtils._hproc;
 
 /**
- * 進入(基本上跟新增頁面同頁)簽核頁面變執行的程式。
- * 主要用於帶出資料庫欄位以外的資料,或是設定哪些欄位隱藏或顯視。
+ * 進入(基本上跟新增頁面同頁)簽核頁面變執行的程式。 主要用於帶出資料庫欄位以外的資料,或是設定哪些欄位隱藏或顯視。
+ * 
  * @author b0050
  *
  */
@@ -14,13 +14,14 @@ public class Init extends _hproc {
 	@Override
 	public String action(String paramString) throws Throwable {
 		// TODO Auto-generated method stub
-		if (getState().equals("挖寶尖兵確認")){
-			setEditable("IS_APPROVE",true);
+		if (POSITION == 5) {
+			if (getState().equals("挖寶尖兵確認")) {
+				setEditable("IS_APPROVE", true);
+			}
+			if (getState().equals("會計主管")) {
+				setEditable("PROJECT_NO", true);
+			}
 		}
-		if (getState().equals("會計主管")){
-			setEditable("PROJECT_NO",true);
-		}
-		
 		return null;
 	}
 
